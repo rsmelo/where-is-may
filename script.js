39a0b9a465aaa02910a9e811750a2e9e66bf225d
rodrigo <rodrigosm@labtrans.ufsc.br>
2017-07-12 17:20:59 -0300

    refactoring js

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
-const overlay = document.getElementById('overlay');
-const may = document.getElementById('may');
-const maya = document.getElementById('maya');
-const title = document.getElementById('title');
-const wrapper = document.getElementById('bkg');
-const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+(function () {
+  const overlay = document.getElementById('overlay');
+  const may = document.getElementById('may');
+  const maya = document.getElementById('maya');
+  const title = document.getElementById('title');
+  const wrapper = document.getElementById('bkg');
+  const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const defaultUrl = `http://loremflickr.com/${width}/${height}/landscape,nopeople,horizontal`;
 
-(function(){   
-    loadImage("");
-
-    document.getElementById("title").addEventListener('click',function (){
-     	location.reload();
-    }); 
-    document.body.addEventListener('keypress',function (evt){
-    	if(evt.keyCode == 27){
-     		var url = prompt("Insira a URL com o novo paraíso das Mays", `http://loremflickr.com/${width}/${height}/landscape,nopeople,horizontal`);
-     		loadImage(url);
-     	}
-    }); 
-})();
-
-function loadImage(url){
-
-	try{
-		var img_loaded = document.getElementById("img-loaded");
-		wrapper.removeChild(img_loaded);
-	}catch(e){}
-
-	const img = new Image();
-
-	load();
-
-	img.onload = function () {               
-        loaded();
-    }
-
-    if(url == ""){
-    	url = `http://loremflickr.com/${width}/${height}/landscape,nopeople,horizontal`;
-    }
-
-    img.classList.add('img-bkg');
-
-    img.src = url;
-    img.id = "img-loaded";
-    
-    wrapper.appendChild(img);
-}
-
-function load(){
-	overlay.classList.add('overlay');
-	may.classList.remove('may');        
+  function showOverlay() {
+    overlay.classList.add('overlay');
+    may.classList.remove('may');
     maya.classList.remove('maya');
     title.classList.remove('title');
-}
+  }
 
-function loaded(){
-	overlay.classList.remove('overlay');        
-    may.classList.add('may');        
-    maya.classList.add('maya');        
+  function hideOverlay() {
+    overlay.classList.remove('overlay');
+    may.classList.add('may');
+    maya.classList.add('maya');
     title.classList.add('title');
-}
+  }
+
+  function loadImage(url = defaultUrl) {
+    const wrapper = document.getElementById('bkg');
+    const img_loaded = document.getElementById("img-loaded");
+    const img = new Image();
+
+    showOverlay();
+
+    img.onload = () => {
+      hideOverlay();
+    };
+
+    img.classList.add('img-bkg');
+    img.src = url;
+    img.id = "img-loaded";
+    wrapper.appendChild(img);
+  }
+
+
+  loadImage();
+  document.getElementById("title").addEventListener('click', () => { location.reload(); });
+
+
+  document.body.addEventListener('keypress', (evt) => {
+    if (evt.keyCode == 27) {
+      const url = prompt("Insira a URL com o novo paraíso das Mays", defaultUrl);
+      loadImage(url);
+    }
+  });
+})();
